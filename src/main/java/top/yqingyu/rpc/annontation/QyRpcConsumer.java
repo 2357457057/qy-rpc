0c4c05865c82b2bd29e72a8bdd7d148b09c7dd2f
SeverusQy <yangyongji123@live.con>
2023-07-08 05:26:16 +0800

    针对Spring代理类进行优化

--- src/main/java/top/yqingyu/rpc/annontation/QyRpcConsumer.java
+++ src/main/java/top/yqingyu/rpc/annontation/QyRpcConsumer.java
@@ -2,12 +2,13 @@ package top.yqingyu.rpc.annontation;
 
 import java.lang.annotation.*;
 
+/**
+ * 配合Spring使用。
+ */
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface QyRpcConsumer {
-    String value();
-
-    String name();
+    String value() default "";
 }
