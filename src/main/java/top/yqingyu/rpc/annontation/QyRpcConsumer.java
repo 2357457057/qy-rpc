42777a1f29a5d30ec93acff1fbfbc5993a980c43
SeverusQy <yangyongji123@live.con>
2023-09-30 19:24:17 +0800

    注解过时

--- src/main/java/top/yqingyu/rpc/annontation/QyRpcConsumer.java
+++ src/main/java/top/yqingyu/rpc/annontation/QyRpcConsumer.java
@@ -3,12 +3,16 @@ package top.yqingyu.rpc.annontation;
 import java.lang.annotation.*;
 
 /**
+ *
  * 配合Spring使用。
+ * 该注解已不再使用
+ * 新版 qyrpc-springboot-starter 已经支持自动注入.
  */
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Deprecated
 public @interface QyRpcConsumer {
     String value() default "";
 }
